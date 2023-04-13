40c79e7d04a48e48d28f6ce8172dab80433c2a45
Felix Ekow Amoquandoh <felix.amoquandoh@amalitech.org>
2023-04-13 17:36:00 +0000

    loading page added

--- worldcountries/src/appPages/Countries/index.tsx
+++ worldcountries/src/appPages/Countries/index.tsx
@@ -1,11 +1,35 @@
 import "./styles.css"
 import { SearchInput } from "../../components/searchInput";
+import { api } from "../../api";
+import { useState } from "react";
+import { useEffect } from 'react'
+
 
 
 export const Countries = () => {
+  const [countries, setCountries] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(()=>{
+    getAllCountries()
+  },[])
+
+  const getAllCountries = async ()=> {
+     setLoading(false)
+     let countries = await api.getCountries()
+     setCountries(countries)
+     setLoading(true)
+  }
+
     return (
         <section className="countriesArea">
           <SearchInput/>  
+          <div className="countries">
+            {loading && <div className="">Loading...</div>}
+            {/* {!loading && countries.map((item, index) =>{
+
+            })} */}
+          </div>
         </section>
     )
 }
--- worldcountries/src/components/searchInput/index.tsx
+++ worldcountries/src/components/searchInput/index.tsx
@@ -3,7 +3,7 @@ import "./styles.css"
 export const SearchInput = () => {
     return (
         <section className="inputArea">
-            <input type="text"/>
+            <input type="text" placeholder="Search by Country"/>
             <select>
                 <option value="Filter by Region" disabled selected>Filter by Region</option>
             </select>
