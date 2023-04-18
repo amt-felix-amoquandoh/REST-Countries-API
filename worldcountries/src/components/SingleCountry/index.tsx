7d7683503c63597bc79634500042a942fb865596
Felix Ekow Amoquandoh <felix.amoquandoh@amalitech.org>
2023-04-18 09:59:14 +0000

    Update index.tsx

--- worldcountries/src/components/SingleCountry/index.tsx
+++ worldcountries/src/components/SingleCountry/index.tsx
@@ -1,48 +1,68 @@
-import "./styles.css"
+import "./styles.css";
 import { SingleCountryTS } from "../../types/SingleCountry";
 
 export const SingleCountry = ({
-    name,
-    nativeName,
-    population,
-    region,
-    subregion,
-    capital,
-    topLevelDomain,
-    currencie,
-    languages,
-    borders,
-    flag
+  name,
+  nativeName,
+  population,
+  region,
+  subregion,
+  capital,
+  topLevelDomain,
+  currencie,
+  languages,
+  borders,
+  flag,
 }: SingleCountryTS) => {
-    return (
-        <div>
-            <img src={flag} alt={`Flag Name: ${name}`} />
-            <div className="countryData">
-                <h1>{name}</h1>
-                <div className="dataBox1">
-                    <p><span>Native Name:</span>{nativeName}</p>
-                    <p><span>Population:</span>{population}</p>
-                    <p><span>Region:</span>{region}</p>
-                    <p><span>Subregion:</span>{subregion}</p>
-                    {
-                        capital && 
-                        <p><span>Capital: </span>{capital}</p>
-                    }
-                    <p className="topLevel"><span>Top Level Domain</span>{topLevelDomain}</p>
-                    {
-                        currencie && <p><span>Currencies: </span>{currencie}</p>
-                    }
-                    <p><span>Languages: </span>{languages}</p>
-                </div>
-            </div>
-            {borders && 
-              <div className="borderArea">
-                <p>Border Countries: </p>
-                <div className="borders">
-                    {borders.map((item, index) => <Link to={`/code/${item}` key{index}}></Link>)}
-                </div>
-              </div>
-            }
+  return (
+    <div>
+      <img src={flag} alt={`Flag Name: ${name}`} />
+      <div className="countryData">
+        <h1>{name}</h1>
+        <div className="dataBox1">
+          <p>
+            <span>Native Name:</span>
+            {nativeName}
+          </p>
+          <p>
+            <span>Population:</span>
+            {population}
+          </p>
+          <p>
+            <span>Region:</span>
+            {region}
+          </p>
+          <p>
+            <span>Subregion:</span>
+            {subregion}
+          </p>
+          {capital && (
+            <p>
+              <span>Capital: </span>
+              {capital}
+            </p>
+          )}
+          <p className="topLevel">
+            <span>Top Level Domain</span>
+            {topLevelDomain}
+          </p>
+          {/* {currencie && (
+            <p>
+              <span>Currencies: </span>
+              {currencie}
+            </p>
+          )} */}
+          {/* <p><span>Languages: </span>{languages}</p> */}
         </div>
-    )
-}
+      </div>
+      {borders && (
+        <div className="borderArea">
+          <p>Border Countries: </p>
+          <div className="borders">
+            {/* {borders.map((item, index) => <Link to={`/code/${item}` key{index}}></Link>)} */}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
