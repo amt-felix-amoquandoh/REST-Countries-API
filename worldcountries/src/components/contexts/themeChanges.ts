2b15d389cd730b152d5602d6a9fce25c1b84d681
Felix Ekow Amoquandoh <felix.amoquandoh@amalitech.org>
2023-05-13 08:03:16 +0000

    Update themeChanges.ts

--- worldcountries/src/components/contexts/themeChanges.ts
+++ worldcountries/src/components/contexts/themeChanges.ts
@@ -1,42 +1,47 @@
-import { type } from "os";
 import { createContext, useContext, useReducer } from "react";
-interface state {
+
+interface State {
   theme: string;
 }
 
-interface actions {
+interface Action {
   type: themeActions;
   payload: any;
 }
 
-const initialData = {
+const initialData: State = {
   theme: "dark",
 };
 
 interface ContextType {
   state: State;
-  dispatch: (action: Actions) => void;
+  dispatch: (action: Action) => void;
 }
 
-const ThemeContext = createContext(undefined);
+const ThemeContext = createContext<ContextType | undefined>(undefined);
 
 export enum themeActions {
   setTheme,
 }
 
-export const reducer = (state: State, action: Actions) => {
+export const reducer = (state: State, action: Action) => {
   switch (action.type) {
     case themeActions.setTheme:
       return { ...state, theme: action.payload };
-      break;
+    default:
+      return state;
   }
 };
 
-export const ThemeProvider = ({ children }: Provider) => {
+interface Provider {
+  children: React.ReactNode;
+}
+
+export const ThemeProvider: React.FC<Provider> = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialData);
   const value = { state, dispatch };
 
   return (
-    <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
+    <ThemeContext.Provider {value={value}}>{children}</ThemeContext.Provider>
   );
 };
