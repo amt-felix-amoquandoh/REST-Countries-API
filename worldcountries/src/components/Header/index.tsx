5184d11b19240151fddc819b7986e2ff18ea1a97
Felix Ekow Amoquandoh <felix.amoquandoh@amalitech.org>
2023-05-17 14:33:14 +0000

    modules update

--- worldcountries/src/components/Header/index.tsx
+++ worldcountries/src/components/Header/index.tsx
@@ -1,13 +1,16 @@
-import "./styles.css"
-import { Moon } from 'react-feather';
+import "./styles.css";
+import { IonIcon } from "@ionic/react";
+import { moonOutline, heart } from "ionicons/icons";
 
 export const Header = () => {
-    return (
-        <header className="header">
-           <div className="container">
-           <h1>Where in the World</h1>
-           <p><Moon /> <b>Dark Mode</b></p>
-           </div>
-        </header>    
-    )
-}
+  return (
+    <header className="header">
+      <div className="container">
+        <h1>Where in the World</h1>
+        <p>
+          <IonIcon icon={moonOutline} /> <b>Dark Mode</b>
+        </p>
+      </div>
+    </header>
+  );
+};
