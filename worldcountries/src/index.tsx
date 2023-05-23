7f07564ae0a417a5b48125d6e62e48113b784b17
Felix Ekow Amoquandoh <felix.amoquandoh@amalitech.org>
2023-05-23 16:10:50 +0000

    app update

--- worldcountries/src/index.tsx
+++ worldcountries/src/index.tsx
@@ -3,17 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Routes } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import { ThemeProvider } from "./contexts/ThemeContext";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider>
-      <Routes>
-        <App />
-      </Routes>
-    </ThemeProvider>
+    <Routes>
+      <App />
+    </Routes>
   </React.StrictMode>
 );
