395cfe36743f96238d14028e02bc04d219b57047
Felix Ekow Amoquandoh <felix.amoquandoh@amalitech.org>
2023-06-06 07:17:15 +0000

    app rsponsiveness

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,14 +1,13 @@
 import React, { useState } from 'react';
 import { MainRoute } from "./AppRoutes/MainRoute";
 import { Header } from "./components/Header";
-import { Moon, Sun } from 'react-feather';
+
 
 function App() {
   const [theme, setTheme] = useState("light");
 
   const toggleTheme = () => {
     setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
-    console.log("hey");
   };
 
   return (
