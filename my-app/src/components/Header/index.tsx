de1ec86e7403694ccab567cfaf4b7cba3ee9127d
Felix Ekow Amoquandoh <felix.amoquandoh@amalitech.org>
2023-06-09 18:25:42 +0000

    Update index.tsx

--- my-app/src/components/Header/index.tsx
+++ my-app/src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Moon, Sun } from "react-feather";
+import { Link } from "react-router-dom";
 import "./styles.css";
 
 type HeaderProps = {
@@ -15,7 +16,9 @@ export const Header = ({ toggleTheme, theme }: HeaderProps) => {
   return (
     <header className="header">
       <div className="container">
-        <h1>Where in the world</h1>
+        <Link to="/">
+          <h1>Where in the world</h1>
+        </Link>
         <p onClick={toggleTheme}>
           {icon} <b>{buttonText}</b>
         </p>
