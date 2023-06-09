7c057d15558e4d033a04d0e422ad16103d33a464
Felix Ekow Amoquandoh <felix.amoquandoh@amalitech.org>
2023-06-09 16:37:26 +0000

    input and filter modified

--- my-app/src/components/Header/index.tsx
+++ my-app/src/components/Header/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Moon, Sun } from 'react-feather';
+import React from "react";
+import { Moon, Sun } from "react-feather";
+import "./styles.css";
 
 type HeaderProps = {
   toggleTheme: () => void;
@@ -7,12 +8,16 @@ type HeaderProps = {
 };
 
 export const Header = ({ toggleTheme, theme }: HeaderProps) => {
+  const isDarkMode = theme === "dark";
+  const buttonText = isDarkMode ? "Light Mode" : "Dark Mode";
+  const icon = isDarkMode ? <Sun /> : <Moon />;
+
   return (
     <header className="header">
       <div className="container">
         <h1>Where in the world</h1>
         <p onClick={toggleTheme}>
-          {theme === 'light' ? <Moon /> : <Sun />} <b>Dark Mode</b>
+          {icon} <b>{buttonText}</b>
         </p>
       </div>
     </header>
